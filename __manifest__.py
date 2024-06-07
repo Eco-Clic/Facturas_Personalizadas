bfa594c53dd9cf4457ee3b38b56c779c27b185de
Eco-Clic <tecnico@eco-clic.net>
2024-06-07 12:13:44 +0200

    __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'TEST FACTURAS',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'Eco-Clic',
+    'category': 'Accounting',
+    'description': """
+    Módulo para facturas y albaranes personalizados
+    """,
+    'data': [
+        'views/boton_facturas_personalizadas.xml',
+        'reports/report_facturas_personalizadas.xml',
+    ],
+    'installable': True,
+}
